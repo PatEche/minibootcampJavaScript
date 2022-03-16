86520882b8ce0e47588ab1e77d253f973e9288d2
Patricio Luis Echeverria <echeverriaple@hotmail.com>
2022-03-16 16:01:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Ejercicios Practicos N° 1 1- Deberán mostrar por consola los siguientes
+datos: 
+a) Mostrar el día de tu cumpleaños (en numero) Ej: 23 
+b) Mostrar el mes de tu cumpleaños (en numero) Ej: 07 
+c) Mostrar el año de tu nacimiento.
+d) Mostrar tu número favorito. */
+
+console.log("Hola Mundo Javascript");
+console.log(8);
+console.log(11);
+console.log(1983);
+console.log(99);
