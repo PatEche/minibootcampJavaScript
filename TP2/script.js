788042321251837b3aa32147331b3645a25968c2
Patricio Luis Echeverria <echeverriaple@hotmail.com>
2022-03-16 17:30:42 -0300

    Add files via upload

---
+++ TP2/script.js
@@ -0,0 +1,20 @@
+var cantidadDeVentanasEnMiCasa = 6;
+console.log("Cantidad de ventanas en casa: " + cantidadDeVentanasEnMiCasa);
+
+var soyHumano = true;
+console.log("Soy Humano: " + soyHumano);
+
+var miGustoDePizzaPreferido = "'Napolitana'";
+console.log("Mi Pizza Favorita es la : " + miGustoDePizzaPreferido);
+
+var otroValor = false;
+console.log(otroValor);
+
+var otroValor = "Hola";
+console.log(otroValor);
+
+var otroValor = 0;
+console.log(otroValor);
+
+var otroValor = "Minions";
+console.log(otroValor);
